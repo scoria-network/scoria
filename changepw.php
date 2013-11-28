9c86f827885e688913349f1546fb78a084337cc6
chinstorff <christopherhinstorff@me.com>
2013-11-28 22:29:07 +0000

    change changepw stuff

---
+++ changepw.php
@@ -0,0 +1,14 @@
+<?php 
+   require('header.php');
+   if(isset($_SESSION['uid'])) {
+      $leftpane = 'panes/'.$_SESSION['leftpane'].'.pane';
+      require($leftpane);
+      require('panes/changepw.pane');
+      require('panes/friends.pane'); 
+   }
+   else {
+      header("Location: /");
+   }
+
+   require_once('footer.php');
+?>
